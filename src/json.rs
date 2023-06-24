297128c0a2f30e9e0ec3f12f03be4dac1b696342
0x2022 <0x2022@gmail.com>
2023-06-25 00:30:09 +0800

    feat: add json and http cli

---
+++ src/json.rs
@@ -0,0 +1,79 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Animal {
+    pub name: String,
+    pub age: u32,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::json::Animal;
+    use tracing::info;
+    use tracing_test::traced_test;
+
+    #[traced_test]
+    #[test]
+    fn struct_to_json() {
+        let tom = Animal {
+            name: "tom".to_string(),
+            age: 20,
+        };
+        let jstr = serde_json::to_string(&tom).unwrap();
+        assert!(jstr.len() > 0);
+    }
+
+    #[test]
+    #[traced_test]
+    fn struct_from_json() {
+        let jstr = "{\"name\":\"tom\",\"age\":20}";
+
+        let cat: Animal = serde_json::from_str(jstr).unwrap();
+        assert_eq!(cat.age, 20);
+    }
+
+    #[test]
+    #[traced_test]
+    fn read_json_file() {
+        let dir = std::env::current_dir().unwrap();
+        let file = dir.join("testdata").join("animals.json");
+        info!(" cur file {:?}", &file);
+        let arr_str = std::fs::read_to_string(file).unwrap();
+        let animals: Vec<Animal> = serde_json::from_str(&arr_str).unwrap();
+        assert_eq!(animals.len(), 2);
+    }
+
+    #[test]
+    #[traced_test]
+    fn map_to_json() {
+        let mut db = HashMap::new();
+        db.insert(
+            "tom",
+            Animal {
+                name: "tom".to_owned(),
+                age: 10,
+            },
+        );
+        db.insert(
+            "jerry",
+            Animal {
+                name: "jerry".to_owned(),
+                age: 12,
+            },
+        );
+        let jstr = serde_json::to_string(&db).unwrap();
+        assert!(jstr.len() > 0);
+    }
+
+    #[test]
+    #[traced_test]
+    fn json_to_map() {
+        let jstr = "{\"name\":\"tom\",\"age\":20}";
+
+        let db: HashMap<String, serde_json::Value> = serde_json::from_str(jstr).unwrap();
+
+        assert_eq!(db.len(), 2);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
+mod http_cli;
+mod json;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
